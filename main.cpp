c597793b172a3fd4e35ee35a4d13c40af159f204
hatim-svg <hatimhafiz2006@gmail.com>
2026-05-31 22:28:32 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,61 @@
-#include "student.h"
-
+#include "LibraryBook.h"
+#include "Functions.h"
+#include <iostream>
+using namespace std;
 int main() {
-
+    LibraryBook library[MAX_BOOKS];
+    int bookCount = 0;
     int choice;
 
-    do {
-        cout << "\n===== STUDENT MANAGEMENT SYSTEM =====\n";
-        cout << "1. Add Student\n";
-        cout << "2. Display Students\n";
-        cout << "3. Search Student\n";
-        cout << "4. Update Student\n";
-        cout << "5. Delete Student\n";
-        cout << "6. Exit\n";
+   
+    library[bookCount++] = LibraryBook(101, "Object Oriented Prog C++", "Kamate Katende");
+    library[bookCount++] = LibraryBook(102, "Data Structures & Algos", "Robert Sedgewick");
 
-        cout << "Enter Your Choice: ";
+    do {
+        cout << "\n=========================================\n";
+        cout << "     LIBRARY BOOK BORROWING SYSTEM       \n";
+        cout << "=========================================\n";
+        cout << "1. Add New Book\n";
+        cout << "2. Display All Books\n";
+        cout << "3. Borrow Book\n";
+        cout << "4. Return Book\n";
+        cout << "5. Search Book by ID\n";
+        cout << "6. Display Overdue Books\n";
+        cout << "7. Calculate Total Fines\n";
+        cout << "8. Exit\n";
+        cout << "-----------------------------------------\n";
+        cout << "Enter your choice (1-8): ";
         cin >> choice;
 
         switch(choice) {
-
             case 1:
-                addStudent();
+                addBook(library, bookCount);
                 break;
-
             case 2:
-                displayStudents();
+                displayBooks(library, bookCount);
                 break;
-
             case 3:
-                searchStudent();
+                borrowBookMenu(library, bookCount);
                 break;
-
             case 4:
-                updateStudent();
+                returnBookMenu(library, bookCount);
                 break;
-
             case 5:
-                deleteStudent();
+                searchBook(library, bookCount);
                 break;
-
             case 6:
-                cout << "Program Ended.\n";
+                displayOverdueBooks(library, bookCount);
                 break;
-
+            case 7:
+                calculateTotalFines(library, bookCount);
+                break;
+            case 8:
+                cout << "Exiting system. Thank you for using our library!\n";
+                break;
             default:
-                cout << "Invalid Choice.\n";
+                cout << "Invalid Option! Please enter a number from 1 to 8.\n";
         }
-
-    } while(choice != 6);
+    } while (choice != 8);
 
     return 0;
 }
